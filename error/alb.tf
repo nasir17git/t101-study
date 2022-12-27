22217891276a9e1689e5366ca2047b2c636cc7e0
nasir17git <nasir17.dev@gmail.com>
2022-12-27 15:31:14 +0900

    error

---
+++ error/alb.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "alb" {
+  name               = "${var.env}-${var.name_prefix}-alb"
+  internal           = var.internal
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.pub[*].id]
+  security_groups    = [aws_security_group.alb.id]
+  tags = {
+    Name = "${var.env}-${var.name_prefix}-alb"
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "${var.env}-${var.name_prefix}-alb"
+  description = "${var.env}-${var.name_prefix}-alb"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env}-${var.name_prefix}-alb"
+  }
+}
