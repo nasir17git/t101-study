4b96456ddcc2e2c9dd187c408a442179fd737833
nasir17git <nasir17.dev@gmail.com>
2022-12-27 16:27:09 +0900

    error 3

--- error/alb.tf
+++ error/alb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "alb" {
   name               = "${var.env}-${var.name_prefix}-alb"
-  internal           = var.internal
+  internal           = false
   load_balancer_type = "application"
   subnets            = [aws_subnet.pub[*].id]
   security_groups    = [aws_security_group.alb.id]
