e72353d09a0eab2f5035e47696729dfd2d09aea1
nasir17git <nasir17.dev@gmail.com>
2022-12-27 15:39:33 +0900

    error

--- error/alb.tf
+++ error/alb.tf
@@ -12,7 +12,7 @@ resource "aws_lb" "alb" {
 resource "aws_security_group" "alb" {
   name        = "${var.env}-${var.name_prefix}-alb"
   description = "${var.env}-${var.name_prefix}-alb"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port   = var.server_port
--- error/variables.tf
+++ error/variables.tf
@@ -13,6 +13,6 @@ variable "pri_subnet_cidrs" {
     2 = "10.0.50.0/24"
   3 = "10.0.60.0/24" }
 }
-
+variable "server_port" { default = 80 }
 # 작동을 위해 필요한 값을 받아오는 data들
 data "aws_availability_zones" "available" { state = "available" }
