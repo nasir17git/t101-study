f1213bfb79e6f83c52c5a44542dd48be594d771b
nasir17git <nasir17.dev@gmail.com>
2023-01-10 12:05:31 +0900

    mk difference2

--- error/alb.tf
+++ error/alb.tf
@@ -1,14 +1,14 @@
-resource "aws_lb" "alb" {
-  name                       = "${var.env}-${var.name_prefix}-alb"
-  internal                   = false
-  load_balancer_type         = "application"
-  subnets                    = tolist([aws_subnet.pub[1].id, aws_subnet.pub[2].id, aws_subnet.pub[3].id])
-  security_groups            = [aws_security_group.alb.id]
-  enable_deletion_protection = false
-  tags = {
-    Name = "${var.env}-${var.name_prefix}-alb"
-  }
-}
+# resource "aws_lb" "alb" {
+#   name                       = "${var.env}-${var.name_prefix}-alb"
+#   internal                   = false
+#   load_balancer_type         = "application"
+#   subnets                    = tolist([aws_subnet.pub[1].id, aws_subnet.pub[2].id, aws_subnet.pub[3].id])
+#   security_groups            = [aws_security_group.alb.id]
+#   enable_deletion_protection = false
+#   tags = {
+#     Name = "${var.env}-${var.name_prefix}-alb"
+#   }
+# }
 
 resource "aws_security_group" "alb" {
   name        = "${var.env}-${var.name_prefix}-alb"
