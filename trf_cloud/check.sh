88f65a25c2e921bc7527c951116658ce0c10d8f7
nasir17git <nasir17.dev@gmail.com>
2023-02-01 18:11:56 +0900

    cleanup rest

--- trf_cloud/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(cat ./full.txt) =~ "Plan: " ]]; then
-  export RESULT=$(sed -n '/Plan:/p' full.txt)
-  echo $RESULT
-else 
-  export RESULT=$(cat full.txt | grep "No changes." | cut -d "." -f1)
-  echo $RESULT
-fi
